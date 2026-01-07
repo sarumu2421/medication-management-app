a7f5e824811ec5c3ea9daa42ed609ccfd7655283
sarumu2421 <146677597+sarumu2421@users.noreply.github.com>
2026-01-07 13:39:13 -0500

    updated CORS origins

--- main.py
+++ main.py
@@ -12,7 +12,11 @@ app = FastAPI(title="Medication Management")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://medication-manag3r.onrender.com",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
